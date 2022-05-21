0ffc0ecc4dfd5651b31459d501556505f81a2109
James Adakole <j.adakole@taxaide.com.ng>
2022-05-22 00:11:47 +0100

    finish test

--- truffle-config.js
+++ truffle-config.js
@@ -1,14 +1,32 @@
+const HDWalletProvider = require('truffle-hdwallet-provider');
+require('dotenv').config();
+
+const mnemonic = process.env.MNEMONIC;
+const infuraKey = process.env.INFURA_KEY;
+
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // for more about customizing your Truffle configuration!
   networks: {
     development: {
-      host: "127.0.0.1",
+      host: '127.0.0.1',
       port: 7545,
-      network_id: "*" // Match any network id
+      network_id: '*', // Match any network id
+    },
+    rinkeby: {
+      host: 'localhost',
+      provider: function() {
+        return new HDWalletProvider(
+          mnemonic,
+          `https://rinkeby.infura.io/v3/${infuraKey}`
+        );
+      },
+        
+      network_id: 4,
+      gas: 4500000,
     },
     develop: {
-      port: 8545
-    }
-  }
+      port: 8545,
+    },
+  },
 };
